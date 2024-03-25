70b988d997bf00bdeedfc4db6e4087f583fbe3b1
tchotaneu <giressetchotaneu@gmail.com>
2024-03-25 16:46:20 +0700

    add description

---
+++ my-market/src/compoments/Description.js
@@ -0,0 +1,6 @@
+function Description() {
+    const text = "Ici achetez tous vous produits dont vous avez toujours rêvées"
+    const emojis = "🤑🤑🤑"
+    return (<p>{ text + emojis }</p>)
+}
+export default  Description
